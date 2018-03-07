7687ede35a04bfa3fa2ef0db4d3d4fe3ae455978
Leon Fedotov <fedotov.leon@gmail.com>
2018-03-08 01:17:16 +0200

    update led

--- led-backpack.js
+++ led-backpack.js
@@ -19,21 +19,14 @@ board.on('ready', function() {
 
     strip.on('ready', function() {
 
-        var colors = ['magenta'];
-        var current_colors = [0,1,2,3,4];
-        var current_pos = [0,1,2,3,4];
+        var colors = ['magenta', 'blue']
+
         var blinker = setInterval(function() {
-            strip.color('#000');
-
-            for (var i=0; i< current_pos.length; i++) {
-                if (++current_pos[i] >= strip.length) {
-                    current_pos[i] = 0;
-                    if (++current_colors[i] >= colors.length) current_colors[i] = 0;
-                }
-                strip.pixel(current_pos[i]).color(colors[current_colors[i]]);
-            }
-
-            strip.show();
-        }, 1000/fps);
+            const current = colors.pop()
+            strip.color(current)
+            colors.shift(current)
+        
+            strip.show()
+        }, 200)
     });
 });
