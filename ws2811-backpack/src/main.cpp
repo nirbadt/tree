395f5d5a7dae3a66f4cc110e2e3a895b021c1a94
Leon Fedotov <fedotov.leon@gmail.com>
2018-03-09 14:27:50 +0200

    progress

--- ws2811-backpack/src/main.cpp
+++ ws2811-backpack/src/main.cpp
@@ -1,9 +1,32 @@
 #include <Arduino.h>
+#include <FastLED.H>
+#include <Wire.h>
+#include <SoftTimer.h>
+#include <DelayRun.h>
 
-void setup() {
-    // put your setup code here, to run once:
+#define I2C_ADDR  0x42
+#define MAX_RECEIVED_BYTES  16
+
+#define LED_DATA_PIN 6
+#define NUM_LEDS 8
+
+CRGB leds[NUM_LEDS];
+
+void turnOn(Task* me) {
+    leds[0] = CRGB::Red;
+    FastLED.show();
 }
 
-void loop() {
-    // put your main code here, to run repeatedly:
+void turnOff(Task* me) {
+    leds[0] = CRGB::Black;
+    FastLED.show();
+}
+
+DelayRun taskOn(200, turnOn);
+DelayRun taskOff(200, turnOff, &taskOn);
+
+void setup() {
+    SoftTimer.add(&taskOn);
+    SoftTimer.add(&taskOff);
+    FastLED.addLeds<WS2811, LED_DATA_PIN, RGB>(leds, NUM_LEDS);
 }
