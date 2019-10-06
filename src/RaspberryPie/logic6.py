785883b5593b2545748bab51cc0f5e2788f591d0
lavron <lavron@lavron.info>
2019-10-06 09:26:33 +0300

    debug added

--- src/RaspberryPie/logic6.py
+++ src/RaspberryPie/logic6.py
@@ -172,4 +172,5 @@ while True:
     remote_touch = 0 if remoteTouchCount < REAL_LEN else remoteTouchCount - REAL_LEN
 
     ser.write("{} {}".format(local_touch, remote_touch))
-    print("{} {}".format(local_touch, remote_touch))
+    print("Sending to teensy: {} {}".format(local_touch, remote_touch))
+    print("Real values:       {} {}".format(touchCount, remoteTouchCount))
